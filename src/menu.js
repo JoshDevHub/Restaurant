e8bc2f227a0466b8fb6116844457dd63d0d4c2f5
jmsmith1018 <jmsmith1018@gmail.com>
2022-12-30 22:08:23 -0700

    refactor: cleanup logic for menu component

--- src/menu.js
+++ src/menu.js
@@ -2,6 +2,32 @@ import { buildElement } from "./dom_helpers";
 
 const entryPoint = document.getElementById("content");
 
+const domTreeForItem = ({name, price}) => {
+  return {
+    attributes: { class: "menu__container__item" },
+    children: [
+      { tag: "p", text: name },
+      { tag: "p", text: price },
+    ]
+  }
+}
+
+const buildMenuSection = (sectionName, ...sectionContent) => {
+  const itemTree = sectionContent.map(domTreeForItem);
+  console.log(itemTree);
+  return {
+    tag: "section",
+    children: [
+      {
+        tag: "h2",
+        text: sectionName,
+      },
+      ...itemTree
+    ]
+  }
+}
+
+
 const menuContent = {
   tag: "section",
   attributes: { class: "menu" },
@@ -12,38 +38,35 @@ const menuContent = {
     },
     {
       attributes: { class: "menu__container" },
+      children: [
+        buildMenuSection(
+          "Brewed Coffee",
+          { name: "Brewed Dark Roast", price: "2.00" },
+          { name: "Brewed Light Roast", price: "2.00" }
+        ),
+        buildMenuSection(
+          "Espresso Drinks",
+          { name: "Americano", price: "2.50" },
+          { name: "Caffe Mocha", price: "3.25" },
+          { name: "Cappuccino", price: "3.25" },
+          { name: "Flat White", price: "3.50" },
+          { name: "Latte", price: "3.25" },
+          { name: "Macchiato", price: "3.00" },
+        ),
+        buildMenuSection(
+          "Cold Coffee",
+          { name: "Cold Brew", price: "3.50" },
+          { name: "Iced Americano", price: "2.75" },
+        )
+      ]
     }
   ]
 }
-
-const menuItems = [
-  { name: "Americano", price: "2.50" },
-  { name: "Brewed Dark Roast", price: "2.00" },
-  { name: "Brewed Light Roast", price: "2.00" },
-  { name: "Caffe Mocha", price: "3.25" },
-  { name: "Cappuccino", price: "3.25" },
-  { name: "Cold Brew", price: "3.50" },
-  { name: "Flat White", price: "3.50" },
-  { name: "Iced Americano", price: "2.50" },
-  { name: "Latte", price: "3.25" },
-  { name: "Macchiato", price: "3.00" },
-]
 
 export default function renderMenu() {
   entryPoint.replaceChildren();
   const fragment = document.createDocumentFragment();
   fragment.appendChild(buildElement(menuContent));
-  const itemsContainer = fragment.querySelector(".menu__container");
-  menuItems.forEach(({name, price}) => {
-    const itemDom = buildElement({
-      attributes: { class: "menu__container__item" },
-      children: [
-        { tag: "p", text: name },
-        { tag: "p", text: price },
-      ]
-    })
-    itemsContainer.appendChild(itemDom);
-  })
 
   entryPoint.appendChild(fragment);
 }
